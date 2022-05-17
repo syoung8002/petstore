c4d7c94fb69eb4d8ca478bc9ec0e96a45db5c5ff
dhtns818 <dhtns818@rosesystems.kr>
2022-05-17 16:04:27 +0900

    commit

---
+++ insurance/src/main/java/petstore/domain/HealthRepository.java
@@ -0,0 +1,8 @@
+
+package petstore.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HealthRepository extends CrudRepository<Health, Long>{    // Repository Pattern Interface
+  
+}
